b5080516ab119e55b98b2bb07d9f1f3f166a175e
Kurobac <kurobac@foxmail.com>
2026-07-24 13:54:17 +0800

    Declare core package platform minimums
    Set the NetToolCore package baselines to macOS 10.15 and iOS 13 so SwiftPM accepts the availability declared by X509 and CryptoKit. The application deployment target remains iOS 26.

--- Packages/NetToolCore/Package.swift
+++ Packages/NetToolCore/Package.swift
@@ -4,6 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "NetToolCore",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
         .library(
             name: "NetToolCore",
